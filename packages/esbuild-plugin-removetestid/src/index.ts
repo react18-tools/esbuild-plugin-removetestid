30f71e53a4f873997325d135287d66bb4abfeecc
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-09-03 10:25:40 +0530

    add plugin

---
+++ packages/esbuild-plugin-removetestid/src/index.ts
@@ -0,0 +1,14 @@
+import { Plugin } from "esbuild";
+import fs from "node:fs";
+
+const removeTestidPlugin: () => Plugin = () => ({
+	name: "esbuild-plugin-removetestid",
+	setup(build) {
+		build.onLoad({ filter: /.*\.tsx$/ }, args => {
+			const text = fs.readFileSync(args.path, "utf8");
+			return { contents: text.replace(/\s*data-testid="[^"]*"/gm, " ") };
+		});
+	},
+});
+
+export = removeTestidPlugin;
