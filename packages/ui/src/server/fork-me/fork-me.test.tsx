4bdbaf3d9f544f628e121ce69a1b0c63cb27b0af
Mayank <mayank.srmu@gmail.com>
2024-01-17 10:52:22 +0000

    Upgrade and fix tests

--- packages/ui/src/server/fork-me/fork-me.test.tsx
+++ packages/ui/src/server/fork-me/fork-me.test.tsx
@@ -2,7 +2,7 @@ import { cleanup, render, screen } from "@testing-library/react";
 import { afterEach, describe, test } from "vitest";
 import { ForkMe } from "./fork-me";
 
-describe.concurrent("fork-me", () => {
+describe("fork-me", () => {
 	afterEach(cleanup);
 
 	test("Smoke test", async ({ expect }) => {
